397a18094551dd69654ee5db60bd93f4198ead02
Brittany Feenstra <brittany.feenstra@formidable.com>
2020-11-23 20:59:43 -0700

    add global style to dashboard to control positioning of snackbar relative to left nav and wordPress nav.

--- assets/src/dashboard/app/index.js
+++ assets/src/dashboard/app/index.js
@@ -38,6 +38,7 @@ import theme, { GlobalStyle } from '../theme';
 import KeyboardOnlyOutline from '../utils/keyboardOnlyOutline';
 import {
   APP_ROUTES,
+  DASHBOARD_LEFT_RAIL_VISIBLE_CLASSNAME,
   NESTED_APP_ROUTES,
   ROUTE_TITLES,
   ADMIN_TITLE,
@@ -79,7 +80,10 @@ const AppContent = () => {
   return (
     <AppFrame>
       {!hideLeftRail && <LeftRail />}
-      <PageContent fullWidth={hideLeftRail}>
+      <PageContent
+        fullWidth={hideLeftRail}
+        className={!hideLeftRail && DASHBOARD_LEFT_RAIL_VISIBLE_CLASSNAME}
+      >
         <Route
           exact
           path={APP_ROUTES.MY_STORIES}
